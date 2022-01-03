e8dfe7c44ac5a799947388571b759674142f24df
Luis Hankel <kaes3kuch3n@gmail.com>
2022-01-03 19:04:33 +0100

    Effects destroyed after 2 seconds

--- Assets/Scripts/PizzaGame/Ingredient.cs
+++ Assets/Scripts/PizzaGame/Ingredient.cs
@@ -5,45 +5,37 @@ namespace PizzaGame
     public class Ingredient : MonoBehaviour
     {
         [SerializeField] private IngredientType type;
-        [SerializeField] static float constraint = -10;
-        public GameObject splashEffect;
-        Rigidbody rb;
+        [SerializeField] private float constraint = -10;
+        [SerializeField] private GameObject splashEffect;
 
         public IngredientType Type => type;
 
         public bool IsInPlace { get; set; }
 
-        private void Start() {
-            rb = GetComponent<Rigidbody>();
-
-        }
-
-        private void Update() {
-            selfDestruction(constraint);
-        }
+        private void Update() => SelfDestruction(constraint);
 
         private void OnCollisionEnter(Collision other)
         {
+            if (!other.gameObject.CompareTag("Pizza"))
+                return;
             // If ingredient lands on pizza, stick it to pizza
-            if (other.gameObject.CompareTag("Pizza"))
-            {
-                transform.parent = other.transform;
-                playSplashEffect();
-            }
-
+            transform.parent = other.transform;
+            PlaySplashEffect();
         }
 
-        private void selfDestruction(float constraint){
-            if(transform.position.y < constraint){
+        private void SelfDestruction(float constraint)
+        {
+            if (transform.position.y < constraint)
                 Destroy(gameObject);
-            }
         }
 
-        public void playSplashEffect(){
-            if(splashEffect != null){
-                Instantiate(splashEffect, transform.position, Quaternion.identity);
+        private void PlaySplashEffect()
+        {
+            if (splashEffect != null)
+            {
+                GameObject effect = Instantiate(splashEffect, transform.position, Quaternion.identity);
+                Destroy(effect, 2);
             }
-           
         }
     }
 }
